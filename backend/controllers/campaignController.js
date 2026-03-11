deb2ab5f69c71e2b6cb12ef5d78c69dfbb1009c5
epicharshgupta <harshitguptapranjul@gmail.com>
2026-03-11 22:05:58 +0530

    saving generated image to db

--- backend/controllers/campaignController.js
+++ backend/controllers/campaignController.js
@@ -1,10 +1,12 @@
 const Campaign = require("../models/Campaign")
+const { generateImage } = require("../services/imageService")
 
 const createCampaign = async (req, res) => {
-
+onsole.log("Generate API hit")
   try {
 
     const { prompt, imageURL, caption } = req.body
+console.log("Prompt:", prompt)
 
     const newCampaign = new Campaign({
       prompt,
@@ -17,11 +19,47 @@ const createCampaign = async (req, res) => {
     res.status(201).json(newCampaign)
 
   } catch (error) {
-
+console.log("Error:", error)
     res.status(500).json({ error: error.message })
 
   }
 
 }
 
-module.exports = { createCampaign }
+const generateCampaign = async (req, res) => {
+
+  try {
+
+    const { prompt } = req.body
+
+    const imageBuffer = await generateImage(prompt)
+
+    const base64Image = Buffer.from(imageBuffer).toString("base64")
+
+    const imageURL = `data:image/png;base64,${base64Image}`
+
+    console.log("Prompt:", prompt)
+    console.log("ImageURL length:", imageURL.length)
+
+    const campaign = new Campaign({
+      prompt,
+      imageURL,
+      caption: "AI generated campaign"
+    })
+
+    await campaign.save()
+
+    res.json({
+      message: "Campaign generated",
+      campaign
+    })
+
+  } catch (error) {
+
+    console.log(error)
+    res.status(500).json({ error: error.message })
+
+  }
+
+}
+module.exports = { createCampaign, generateCampaign }
