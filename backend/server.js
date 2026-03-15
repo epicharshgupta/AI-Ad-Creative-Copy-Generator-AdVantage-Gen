9111f4c87361385c18a2e88fc0731467d26cb8f2
epicharshgupta <harshitguptapranjul@gmail.com>
2026-03-15 12:20:37 +0530

    updation in server file

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
 const express = require("express")
 const cors = require("cors")
-const dotenv = require("dotenv")
 const connectDB = require("./config/db")
 const campaignRoutes = require("./routes/campaignRoutes")
-
-dotenv.config()
 
 connectDB()
 
@@ -12,12 +12,17 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-
+// app.use((req, res, next) => {
+//   console.log("Incoming request:", req.method, req.url)
+//   next()
+// })
 app.get("/", (req, res) => {
   res.send("AdVantage Gen Backend Running")
 })
 
+/* routes */
+app.use("/api/campaign", campaignRoutes)
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
 })
-app.use("/api/campaign", campaignRoutes)
