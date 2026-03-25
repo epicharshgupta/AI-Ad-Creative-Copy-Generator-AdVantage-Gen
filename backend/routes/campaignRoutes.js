98173ca054d2e33116789f926858d685fc4115ac
epicharshgupta <harshitguptapranjul@gmail.com>
2026-03-25 22:35:46 +0530

    feat : adding routes of delete campaign get campaign and make it favourite route

--- backend/routes/campaignRoutes.js
+++ backend/routes/campaignRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const router = express.Router()
 
-const { createCampaign, generateCampaign, getCampaigns } = require("../controllers/campaignController")
+const { createCampaign, generateCampaign, getCampaigns, deleteCampaign, toggleFavorite, getFavorites } = require("../controllers/campaignController")
 
 router.post("/create", createCampaign)
 router.post("/generate", generateCampaign)
 
 router.get("/history", getCampaigns)
-
+router.delete("/:id", deleteCampaign)
+router.patch("/:id/favorite", toggleFavorite)
+router.get("/favorites", getFavorites)
 module.exports = router
